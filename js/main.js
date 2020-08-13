398bd15150fbef52e0f7b24606b83b6028b20810
Eng.MuuhameedUssama <69601947+MuuhameedUssama@users.noreply.github.com>
2020-08-13 21:43:39 +0200

    index Counter Config. Done

--- js/main.js
+++ js/main.js
@@ -102,41 +102,6 @@ $(document).ready(function() {
 	}
 	searchToggle();
 	/*------------------------------------------
-			MATCH COUNTER
-	------------------------------------------*/
-	function matchCounter() {
-		var launch = new Date('2020', '8', '8', '8', '844');
-		var days = $('.tg-days');
-		var hours = $('.tg-hours');
-		var minutes = $('.tg-minutes');
-		var seconds = $('.tg-seconds');
-		setDate();
-		function setDate() {
-			var now = new Date();
-			if (launch < now) {
-				days.html('<h3>0</h3><h4>Day</h4>');
-				hours.html('<h3>0</h3><h4>Hour</h4>');
-				minutes.html('<h3>0</h3><h4>Minute</h4>');
-				seconds.html('<h3>0</h3><h4>Second</h4>');
-			}
-			else {
-				var s = -now.getTimezoneOffset() * 60 + (launch.getTime() - now.getTime()) / 1000;
-				var d = Math.floor(s / 86400);
-				days.html('<h3>' + d + '</h3><h4>Day' + (d > 1 ? 's' : ''), '</h4>');
-				s -= d * 86400;
-				var h = Math.floor(s / 3600);
-				hours.html('<h3>' + h + '</h3><h4>Hour' + (h > 1 ? 's' : ''), '</h4>');
-				s -= h * 3600;
-				var m = Math.floor(s / 60);
-				minutes.html('<h3>' + m + '</h3><h4>Minute' + (m > 1 ? 's' : ''), '</h4>');
-				s = Math.floor(s - m * 60);
-				seconds.html('<h3>' + s + '</h3><h4>Second' + (s > 1 ? 's' : ''), '</h4>');
-				setTimeout(setDate, 1000);
-			}
-		}
-	}
-	matchCounter();
-	/*------------------------------------------
 			PRICE RANGE
 	------------------------------------------*/
 	$(function() {
